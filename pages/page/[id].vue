a684bfec4ff39d48920a24d3dade68515ea15134
moebiusmania <moebiusmania@gmail.com>
2023-03-25 22:01:53 +0100

    fix: pagination route 404

--- pages/page/[id].vue
+++ pages/page/[id].vue
@@ -1,7 +1,7 @@
 <script setup>
 const route = useRoute()
 
-const { data } = useFetch(`https://rickandmortyapi.com/api/character?page=${route.params.index}`, {
+const { data } = useFetch(`https://rickandmortyapi.com/api/character?page=${route.params.id}`, {
   default: () => ({ results: [], info: {} })
 })
 </script>
